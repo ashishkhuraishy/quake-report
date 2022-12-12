26abd61757e0595f38023b5f12d264e11948b92d
ashishkhuraishy <ashishkhuraishy@gmail.com>
2022-12-12 23:24:22 +0530

    feat: basic api call

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+   import type { PageData } from "./$types"; 
+
+   export let data: PageData;
+</script>
+
+
+{#each data.reports as report }
+   <p> {report.id} - {report.place} - {report.mag} </p> 
+{/each}
---
+++ src/routes/+page.ts
@@ -0,0 +1,39 @@
+import type { PageLoad } from "./$types";
+
+const url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02";
+
+interface QuakeInfo {
+    id: string;
+    mag: number;
+    place: string;
+    title: string;
+    time: number;
+    updated_at: number;
+}
+
+export const load: PageLoad = async () => {
+   const res = await fetch(url); 
+   const data = await res.json();
+
+   // eslint-disable-next-line @typescript-eslint/no-explicit-any
+   const features: Array<any> = data.features;
+   
+   const reports = features.map((feature) => {
+    const property = feature.properties;
+    
+    const info: QuakeInfo = {
+        id: feature.id,
+        mag: property.mag,
+        place: property.place,
+        time: property.time,
+        title: property.title,
+        updated_at: property.updated,
+    };
+
+    return info;
+   })
+
+   return {
+    reports: reports
+   };
+};
