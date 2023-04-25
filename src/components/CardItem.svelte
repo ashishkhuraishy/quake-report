3c05f38798fc9a1537387a7b4764059e33154eb9
ashishkhuraishy <ashishkhuraishy@gmail.com>
2023-04-25 20:49:45 +0530

    feat: fixed css issues

--- src/components/CardItem.svelte
+++ src/components/CardItem.svelte
@@ -2,12 +2,20 @@
 	import type { QuakeInfo } from "src/routes/+page";
 
     export let report: QuakeInfo;
+    var style = `flex justify-center items-center rounded-full p-4 ${"bg-red-" + (Math.floor(Math.abs(report.mag) + 1) * 100)} text-sm w-10 h-10 ${report.mag < 3 ? 'text-black' : 'text-white'}`
+
+
+    $: date = new Date(report.time)
 </script>
 
 
 <div class="p-4 bg-neutral-200 rounded-xl my-5">
     <div class="flex flex-auto items-center space-x-4">
-        <div class="flex justify-center items-center rounded-full bg-green-600 p-4 text-sm w-10 h-10 text-white">{report.mag}</div>
-        <h5 class="text-sm">{report.place}</h5>
+        <div class={style} >{report.mag}</div>
+        <div class="text-start">
+            <h5 class="text-sm font-bold">{report.place}</h5>
+            <!-- get date from epoch time -->
+            <div class="text-xs">{date.toString()}</div>
+        </div>
     </div>
 </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,4 +5,6 @@ module.exports = {
     extend: {},
   },
   plugins: [],
+  safelist: ['bg-red-500', 'bg-red-700',  'bg-red-900', 'bg-red-400', 'bg-red-600', 'bg-red-200', 'bg-red-300', 'bg-red-50', 'bg-red-100', 'text-white', 'text-black'],
+  
 }
